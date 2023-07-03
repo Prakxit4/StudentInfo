9ca060936bbc59694255f9450f793e3d9fdeed20
prakshit poudel <prakxit123@gmail.com>
2023-07-03 17:06:53 +0545

    fixed bugs

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,6 @@
 <head>
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <!-- Add your additional CSS links here -->
 </head>
 <body>
     <!-- Navigation bar -->
